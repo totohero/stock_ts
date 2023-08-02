d1d18374bc6b75e44b17d8b24f4c933212396b16
Sungtaek Lim <totohero@gmail.com>
2023-08-03 00:34:46 +0900

    매수/매도 분리 처리

--- backtest.py
+++ backtest.py
@@ -30,25 +30,24 @@ class Backtest:
             shares_to_buy = np.floor(investment / row['close'])
             actual_investment = shares_to_buy * row['close']
             if shares_to_buy > 0:
-                return True, actual_investment
+                return True, actual_investment, shares_to_buy
             else:
-                return False, 0
+                return False, 0, 0
         else:
-            return False, 0
+            return False, 0, 0
 
-
-    def execute_sell(self, df, row, entry_price):
+    def execute_sell(self, df, row, entry_price, shares):
         if row['in_trade']:
             if self.stop_loss is not None and row['low'] / entry_price - 1 <= self.stop_loss:
-                return False, self.stop_loss
+                return False, self.stop_loss * shares * entry_price
             elif self.target_return is not None and row['high'] / entry_price - 1 >= self.target_return:
-                return False, self.target_return
-            elif self.hold_days is not None and row['in_trade'].shift(self.hold_days):
+                return False, self.target_return * shares * entry_price
+            elif self.hold_days is not None and row['in_trade']:
                 pnl = row['close'] / entry_price - 1
-                return False, pnl
+                return False, pnl * shares * entry_price
             elif row['sell_signal']:
                 pnl = row['close'] / entry_price - 1
-                return False, pnl
+                return False, pnl * shares * entry_price
         return row['in_trade'], row['pnl']
 
     def run(self, df, cash):
@@ -56,28 +55,50 @@ class Backtest:
         result_df['in_trade'] = False
         result_df['pnl'] = 0.0
         result_df = self.buy_strategy(result_df)
+        buy_signals = result_df[result_df['buy_signal']].groupby(result_df[result_df['buy_signal']].index)['ticker'].apply(set)
+
+
         result_df = self.sell_strategy(result_df)
+
+        holdings = set()
+        shares_held = {}
 
         for date in result_df.index.unique():
             date_df = result_df.loc[date]
-            for ticker in date_df['ticker'].unique():
-                if cash <= 100000:
-                    break
+            tickers_to_buy = buy_signals.get(date, set())
+            
+            # Buying process
+            for ticker in tickers_to_buy:
+                if ticker not in holdings:
+                    ticker_df = date_df[date_df['ticker'] == ticker].copy()
+
+                    for i, row in ticker_df.iterrows():
+                        in_trade, investment, shares = self.execute_buy(ticker_df, row, cash)
+                        if in_trade and investment > 0:
+                            cash -= investment
+                            holdings.add(ticker)
+                            shares_held[ticker] = shares
+                            ticker_df.loc[i, 'in_trade'] = in_trade
+                            ticker_df.loc[i, 'pnl'] = 0  # No profit/loss at the moment of buying
+                        result_df.loc[(date_df['ticker'] == ticker).index] = ticker_df
+
+            # Selling process
+            tickers_to_remove = set()
+            for ticker in holdings:
                 ticker_df = date_df[date_df['ticker'] == ticker].copy()
-                entry_price = 0.0
-                investment = 0.0
+                entry_price = ticker_df['close'].iloc[0]  # Assuming that we bought at the opening price
+                shares = shares_held[ticker]
+
                 for i, row in ticker_df.iterrows():
-                    in_trade, investment = self.execute_buy(ticker_df, row, cash)
-                    if in_trade and investment > 0:
-                        entry_price = row['close']
-                        cash -= investment
-                    in_trade, pnl = self.execute_sell(ticker_df, row, entry_price)
-                    if not in_trade and investment > 0:
-                        cash += investment
-                        investment = 0
-                    ticker_df.loc[i, 'in_trade'] = in_trade
-                    ticker_df.loc[i, 'pnl'] = pnl * investment
-                result_df.loc[(date_df['ticker'] == ticker).index] = ticker_df
+                    in_trade, pnl = self.execute_sell(ticker_df, row, entry_price, shares)
+                    if not in_trade:
+                        cash += row['close'] * shares
+                        tickers_to_remove.add(ticker)
+                        ticker_df.loc[i, 'in_trade'] = in_trade
+                        ticker_df.loc[i, 'pnl'] = pnl
+                    result_df.loc[(date_df['ticker'] == ticker).index] = ticker_df
+            holdings -= tickers_to_remove
+
             print(f'{date}: Remaining cash is {cash}')
         return result_df, cash
 
