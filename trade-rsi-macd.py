d8832ad0cf978e028b91e03a80459647a2ed5764
Sungtaek Lim <totohero@gmail.com>
2023-08-02 13:56:16 +0900

    add rsi strategy

--- trade-rsi-macd.py
+++ trade-rsi-macd.py
@@ -17,7 +17,7 @@ df['date'] = pd.to_datetime(df['date'])
 
 df.set_index('date', inplace=True)
 
-# Remove rows where 'open' or 'close' is zero
+# Remove rows where 'open' or 'close' or 'high' or 'low' or 'volume' is zero
 df = df[(df['open'] != 0) & (df['close'] != 0) & (df['high'] != 0) & (df['low'] != 0) & (df['volume'] != 0)]
 df_ticker = df.groupby('ticker')
 
@@ -33,8 +33,11 @@ df['MA20'] = df_ticker['close'].rolling(window=20).mean().reset_index(0, drop=Tr
 df['open_close_change_ratio'] = df['close'] / df['open']
 
 # Calculate the next day change ratio from open to close
-df['next_day_open_close_change_ratio'] = df_ticker['close'].shift(-1) / df_ticker['open'].shift(-1)
-df = df.dropna(subset=['next_day_open_close_change_ratio'])
+df['+1d_open_close_change_ratio'] = df_ticker['close'].shift(-1) / df_ticker['open'].shift(-1)
+# df = df.dropna(subset=['+1d_open_close_change_ratio'])
+df['+5d_open_close_change_ratio'] = df_ticker['close'].shift(-5) / df_ticker['open'].shift(-1)
+# df = df.dropna(subset=['+5d_open_close_change_ratio'])
+
 
 df = df[(df['MA5'] > df['MA10']) & (df['MA10'] > df['MA20'])]
 
@@ -44,27 +47,53 @@ df_top_10_percent_turnover = df[df.groupby('date')['transaction_amount'].transfo
 
 def plot_histogram(df, ax, filter_func, title):
     df_filtered = df[filter_func(df)]
-    ax.hist(df_filtered['next_day_open_close_change_ratio'].dropna(), bins=60, color='r', alpha=0.5, edgecolor='black', range=(0.7, 1.3))
+    ax.hist(df_filtered['+5d_open_close_change_ratio'].dropna(), bins=60, color='r', alpha=0.5, edgecolor='black', range=(0.7, 1.3))
     ax.set_title(title)
     ax.set_xlabel('Change Ratio')
     ax.set_ylabel('Frequency')
 
-    avg_change_ratio = df_filtered['next_day_open_close_change_ratio'].mean()
+    avg_change_ratio = df_filtered['+1d_open_close_change_ratio'].mean()
     print(f"Average Change Ratio for {title}: {avg_change_ratio:.4f}")
 
-fig, axs = plt.subplots(1, 4, figsize=(18, 6))
+fig, axs = plt.subplots(1, 5, figsize=(18, 6))
 
 filter_func = lambda df: df['open_close_change_ratio'].notna()
-plot_histogram(df, axs[0], filter_func, 'Open to Close Change Ratio')
+plot_histogram(df, axs[0], filter_func, 'Day0 Change Ratio')
 
 filter_func = lambda df: (df['close'] == df['max_close_20']) & (df['volume'] == df['max_volume_20']) & (df['open_close_change_ratio'] > 1.1)
-plot_histogram(df, axs[1], filter_func, 'Next Day Open to Close Change Ratio\n(Close == Max Close Price)\n(Volume == Max Volume Price)\n(Open to Close Change Ratio > 1.1)')
+plot_histogram(df, axs[1], filter_func, 'Change Ratio\n(Close == Max Close Price)\n(Volume == Max Volume Price)\n(Open to Close Change Ratio > 1.1)')
 
 filter_func = lambda df: df['close'] > 0
-plot_histogram(df, axs[2], filter_func, 'Next Day Open to Close Change Ratio\n(MA5 > MA10 > MA20)')
+plot_histogram(df, axs[2], filter_func, 'Change Ratio\n(MA5 > MA10 > MA20)')
 
 filter_func = lambda df: df['close'] > 0
-plot_histogram(df_top_10_percent_turnover, axs[3], filter_func, 'Top 10 % turnover. Next Day Open to Close Change Ratio\n(MA5 > MA10 > MA20)')
+plot_histogram(df_top_10_percent_turnover, axs[3], filter_func, 'Top 10 % turnover. Change Ratio\n(MA5 > MA10 > MA20)')
+
+# Define the function to calculate RSI
+def calculate_rsi(df, window):
+    delta = df['close'].diff()
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    average_gain = up.rolling(window).mean()
+    average_loss = abs(down.rolling(window).mean())
+    rs = average_gain / average_loss
+    df['RSI'] = 100 - (100 / (1 + rs))
+
+# Define the function to calculate MACD
+def calculate_macd(df, short_window, long_window):
+    short_ema = df['close'].ewm(span=short_window, adjust=False).mean()
+    long_ema = df['close'].ewm(span=long_window, adjust=False).mean()
+    macd_line = short_ema - long_ema
+    signal_line = macd_line.ewm(span=9, adjust=False).mean()
+    df['MACD'] = macd_line - signal_line
+
+# Calculate RSI and MACD
+calculate_rsi(df, window=14)
+calculate_macd(df, short_window=12, long_window=26)
+
+filter_func = lambda df: (df['RSI'] > 70) & (df['MACD'] > 0)
+plot_histogram(df, axs[4], filter_func, 'RSI > 70 and MACD > 0')
 
 plt.tight_layout()
 plt.show()
