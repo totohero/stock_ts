cb2f6a608e80f682bb5b66196b7fec55ddaa31af
Sungtaek Lim <totohero@gmail.com>
2023-08-13 01:17:49 +0900

    hhh

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ import sqlite3
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Connect to the SQLite database
-conn = sqlite3.connect('stock_prices.db')
+@st.cache_data
+def load_data():
+    conn = sqlite3.connect('stock_prices.db')
+    df = pd.read_sql('SELECT * FROM prices', conn)
+    df['date'] = pd.to_datetime(df['date'])
+    return df
 
-# Read the DataFrame from the SQLite database
-df = pd.read_sql('SELECT * FROM prices', conn)
-df['date'] = pd.to_datetime(df['date'])
+df = load_data()
 
 # Sidebar date selection
 min_date = df['date'].min().date()
@@ -20,32 +23,42 @@ end_date = st.sidebar.date_input('End Date', min_value=min_date, max_value=max_d
 # Filter the DataFrame by the selected date range
 df = df[(df['date'] >= pd.Timestamp(start_date)) & (df['date'] <= pd.Timestamp(end_date))]
 
-# Apply additional conditions
-# Calculate the rolling 20 days max close price and max volume
-window = 20
-df['rolling_max_close'] = df.groupby('ticker')['close'].rolling(window=window).max().reset_index(0, drop=True)
-df['rolling_max_volume'] = df.groupby('ticker')['volume'].rolling(window=window).max().reset_index(0, drop=True)
+# Calculate the previous close price
+df['prev_close'] = df.groupby('ticker')['close'].shift(1)
 
-# Filter based on conditions
-df_filtered = df[
-    (df['close'] == df['rolling_max_close']) &
-    (df['volume'] == df['rolling_max_volume']) &
-    ((df['close'] / df['open']) >= 1.07)
-]
+# Apply the condition
+df_filtered = df[df['open'] <= df['prev_close'] * 0.95]
 
-# Calculate the next day open to close change ratio
-df_filtered['next_day_open_close_change_ratio'] = df_filtered.groupby('ticker')['close'].shift(-1) / df_filtered.groupby('ticker')['open'].shift(-1)
-df_filtered['open_close_change_ratio'] = df_filtered['close'] / df_filtered['open']
-df_filtered = df_filtered.dropna(subset=['next_day_open_close_change_ratio', 'open_close_change_ratio'])
+# Calculate the change ratios
+df_filtered['close_change_ratio'] = 100 * df_filtered['close'] / (df_filtered['prev_close'] * 0.95) - 100
+df_filtered['low_change_ratio'] = 100 * df_filtered['low'] / (df_filtered['prev_close'] * 0.95) - 100
+df_filtered.dropna(inplace=True)
 
-# Create a 2D heatmap
+# 위에 위치할 텍스트
+st.header("Top Section")
+st.write("This is the top section of the page.")
+
+# Create a 2D histogram
 plt.figure(figsize=(10, 4))
-sns.kdeplot(data=df_filtered, x='next_day_open_close_change_ratio', y='open_close_change_ratio', cmap="Blues", fill=True)
-plt.xlim(0.9, 1.1)
-plt.ylim(1, 1.2)
-plt.title('2D Heatmap of Change Ratios')
-plt.xlabel('Next Day Open to Close Change Ratio')
-plt.ylabel('Open to Close Change Ratio')
+sns.histplot(data=df_filtered, x='close_change_ratio', y='low_change_ratio', bins=10, cmap="Blues")
+plt.xlim(-10, 10)
+plt.ylim(-10, 0)
+plt.title('2D Histogram of Change Ratios')
+plt.xlabel('Close Change Ratio (5% below previous close)')
+plt.ylabel('Low Change Ratio (5% below previous close)')
+
+st.pyplot(plt.gcf())
+
+# Binning을 적용하여 2D 히스토그램을 계산합니다.
+# close_change와 low_change를 빈으로 나눕니다.
+df_filtered['close_change_bin'] = pd.cut(df_filtered['close_change_ratio'], np.linspace(-10, 10, 11))
+df_filtered['low_change_bin'] = pd.cut(df_filtered['low_change_ratio'], np.linspace(-10, 0, 11))
+
+# 피벗 테이블을 사용하여 빈도를 계산합니다.
+frequency_table = pd.pivot_table(df_filtered, index='low_change_bin', columns='close_change_bin', values='open', aggfunc='count', fill_value=0)
+
+# 표를 출력합니다.
+print(frequency_table)
 
 # Display the plot in Streamlit
-st.pyplot(plt.gcf())
+st.table(frequency_table)
--- 전날종가-시가-종가-상관관계.py
+++ 전날종가-시가-종가-상관관계.py
@@ -19,28 +19,23 @@ df_ticker = df.groupby('ticker')
 
 df['prev_close'] = df['close'].shift(1)
 df['open_change'] = (df['open'] - df['prev_close']) / df['prev_close']
-df['high_change'] = 100*(df['close'] - df['open']) / (df['prev_close'] *0.95)
-df['low_change'] = 100*(df['low'] - df['open']) / (df['prev_close']*0.95)
+df['buy'] = df['prev_close'] * 0.95
+df['high_change'] = 100 * (df['high'] - df['buy'] ) / df['buy']
+df['low_change'] = 100 * (df['low'] - df['buy'] ) / df['buy']
+df.dropna(inplace=True)
 
 # 전날 종가 대비 당일 시가가 5% 이상 하락한 날들을 선택합니다.
-df_significant_drop = df[df['open_change'] <= -0.05]
-
-# -2에서 2 사이의 high_change와 low_change를 제외합니다.
-# filtered_df = df_significant_drop[(df_significant_drop['high_change'] < -2) | (df_significant_drop['high_change'] > 2) | (df_significant_drop['low_change'] < -2) | (df_significant_drop['low_change'] > 2)]
-filtered_df = df_significant_drop
-
-# 빈의 경계를 정의합니다. 여기서는 예를 들어 -30에서 30 사이에 100개의 빈을 사용합니다.
+filtered_df = df[df['open_change'] <= -0.05]
 
 # high_change와 low_change를 빈으로 나눕니다.
-filtered_df['high_change_bin'] = pd.cut(filtered_df['high_change'], np.linspace(0, 20, 11))
-filtered_df['low_change_bin'] = pd.cut(filtered_df['low_change'], np.linspace(-20, 0, 11))
+filtered_df['high_change_bin'] = pd.cut(filtered_df['high_change'], np.linspace(0, 20, 21))
+filtered_df['low_change_bin'] = pd.cut(filtered_df['low_change'], np.linspace(-20, 0, 21))
 
 # 피벗 테이블을 사용하여 빈도를 계산합니다.
 frequency_table = pd.pivot_table(filtered_df, index='low_change_bin', columns='high_change_bin', values='open', aggfunc='count', fill_value=0)
 
 # 표를 출력합니다.
 print(frequency_table)
-
 
 import seaborn as sns
 
