e4edb8016d77e2ff79284fe24685aaa4d96e1780
Sungtaek Lim <totohero@gmail.com>
2023-08-13 01:45:04 +0900

    ㅗㅡㅡㅗ

--- app.py
+++ app.py
@@ -23,16 +23,24 @@ end_date = st.sidebar.date_input('End Date', min_value=min_date, max_value=max_d
 # Filter the DataFrame by the selected date range
 df = df[(df['date'] >= pd.Timestamp(start_date)) & (df['date'] <= pd.Timestamp(end_date))]
 
-# Calculate the previous close price
-df['prev_close'] = df.groupby('ticker')['close'].shift(1)
+# Remove rows where 'open' or 'close' is zero
+df = df[(df['open'] != 0) & (df['close'] != 0) & (df['high'] != 0) & (df['low'] != 0) & (df['volume'] != 0)]
+df_ticker = df.groupby('ticker')
 
-# Apply the condition
-df_filtered = df[df['open'] <= df['prev_close'] * 0.95]
+df['prev_close'] = df['close'].shift(1)
+df['prev_close-low'] = (df['low'] - df['prev_close']) / df['prev_close']
+df['buy'] = df['prev_close'] * 0.9
+df['close_change'] = 100 * (df['close'] - df['buy'] ) / df['buy']
+df['low_change'] = 100 * (df['low'] - df['buy'] ) / df['buy']
+df.dropna(inplace=True)
 
-# Calculate the change ratios
-df_filtered['close_change_ratio'] = 100 * df_filtered['close'] / (df_filtered['prev_close'] * 0.95) - 100
-df_filtered['low_change_ratio'] = 100 * df_filtered['low'] / (df_filtered['prev_close'] * 0.95) - 100
-df_filtered.dropna(inplace=True)
+# 전날 종가 대비 10% 이상 하락한 날들을 선택합니다.
+df_filtered = df[df['prev_close-low'] <= -0.1]
+
+# close_change와 low_change를 빈으로 나눕니다.
+df_filtered['close_change_bin'] = pd.cut(df_filtered['close_change'], np.linspace(-20, 20, 41))
+df_filtered['low_change_bin'] = pd.cut(df_filtered['low_change'], np.linspace(-20, 0, 21))
+
 
 # 위에 위치할 텍스트
 st.header("Top Section")
@@ -40,25 +48,17 @@ st.write("This is the top section of the page.")
 
 # Create a 2D histogram
 plt.figure(figsize=(10, 4))
-sns.histplot(data=df_filtered, x='close_change_ratio', y='low_change_ratio', bins=10, cmap="Blues")
-plt.xlim(-10, 10)
-plt.ylim(-10, 0)
+sns.histplot(data=df_filtered, x='close_change', y='low_change', bins=100, cmap="Blues")
+plt.xlim(-20, 20)
+plt.ylim(-20, 0)
 plt.title('2D Histogram of Change Ratios')
 plt.xlabel('Close Change Ratio (5% below previous close)')
 plt.ylabel('Low Change Ratio (5% below previous close)')
-
 st.pyplot(plt.gcf())
-
-# Binning을 적용하여 2D 히스토그램을 계산합니다.
-# close_change와 low_change를 빈으로 나눕니다.
-df_filtered['close_change_bin'] = pd.cut(df_filtered['close_change_ratio'], np.linspace(-10, 10, 11))
-df_filtered['low_change_bin'] = pd.cut(df_filtered['low_change_ratio'], np.linspace(-10, 0, 11))
 
 # 피벗 테이블을 사용하여 빈도를 계산합니다.
 frequency_table = pd.pivot_table(df_filtered, index='low_change_bin', columns='close_change_bin', values='open', aggfunc='count', fill_value=0)
 
 # 표를 출력합니다.
+# st.table(frequency_table)
 print(frequency_table)
-
-# Display the plot in Streamlit
-st.table(frequency_table)
--- 전날종가-시가-종가-상관관계.py
+++ 전날종가-시가-종가-상관관계.py
@@ -18,21 +18,21 @@ df = df[(df['open'] != 0) & (df['close'] != 0) & (df['high'] != 0) & (df['low'] 
 df_ticker = df.groupby('ticker')
 
 df['prev_close'] = df['close'].shift(1)
-df['open_change'] = (df['open'] - df['prev_close']) / df['prev_close']
-df['buy'] = df['prev_close'] * 0.95
-df['high_change'] = 100 * (df['high'] - df['buy'] ) / df['buy']
+df['prev_close-low'] = (df['low'] - df['prev_close']) / df['prev_close']
+df['buy'] = df['prev_close'] * 0.9
+df['close_change'] = 100 * (df['close'] - df['buy'] ) / df['buy']
 df['low_change'] = 100 * (df['low'] - df['buy'] ) / df['buy']
 df.dropna(inplace=True)
 
-# 전날 종가 대비 당일 시가가 5% 이상 하락한 날들을 선택합니다.
-filtered_df = df[df['open_change'] <= -0.05]
+# 전날 종가 대비 10% 이상 하락한 날들을 선택합니다.
+filtered_df = df[df['prev_close-low'] <= -0.1]
 
-# high_change와 low_change를 빈으로 나눕니다.
-filtered_df['high_change_bin'] = pd.cut(filtered_df['high_change'], np.linspace(0, 20, 21))
+# close_change와 low_change를 빈으로 나눕니다.
+filtered_df['close_change_bin'] = pd.cut(filtered_df['close_change'], np.linspace(0, 20, 21))
 filtered_df['low_change_bin'] = pd.cut(filtered_df['low_change'], np.linspace(-20, 0, 21))
 
 # 피벗 테이블을 사용하여 빈도를 계산합니다.
-frequency_table = pd.pivot_table(filtered_df, index='low_change_bin', columns='high_change_bin', values='open', aggfunc='count', fill_value=0)
+frequency_table = pd.pivot_table(filtered_df, index='low_change_bin', columns='close_change_bin', values='open', aggfunc='count', fill_value=0)
 
 # 표를 출력합니다.
 print(frequency_table)
@@ -42,7 +42,7 @@ import seaborn as sns
 # 히트맵을 그립니다.
 plt.figure(figsize=(10, 8))
 sns.heatmap(frequency_table, cmap="YlGnBu", annot=True)
-plt.title('High Change vs Low Change Frequency')
-plt.xlabel('High Change')
+plt.title('Close Change vs Low Change Frequency')
+plt.xlabel('Close Change')
 plt.ylabel('Low Change')
 plt.show()
