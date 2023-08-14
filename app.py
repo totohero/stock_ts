1460883760ad66d8782d8a3ec5b1f77fa6303a49
totohero <totohero@gmail.com>
2023-08-14 20:42:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import crawl_sqlite3
 import analysis_1day
 import analysis_5day
 
+st.sidebar.title("Choose action:")
+b1 = st.sidebar.button("Crawl data (could be very slow)"):
+b2 = st.sidebar.button("1 Day Analysis"):
+b3 = st.sidebar.button("5 Day Analysis"):
+
 try:
     st.sidebar.title("Choose action:")
-    if st.sidebar.button("Crawl data (could be very slow)"):
+    if b1:
         crawl_sqlite3.crawl()
-    if st.sidebar.button("1 Day Analysis"):
+    if b2:
         analysis_1day.doit()
-    if st.sidebar.button("5 Day Analysis"):
+    if b3:
         analysis_5day.doit()
 except Exception as e:
     st.error(e)
