7921aae1d8e6fe89e43e95dac7fc2c6ffae3a31d
totohero <totohero@gmail.com>
2023-08-14 20:42:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import analysis_1day
 import analysis_5day
 
 st.sidebar.title("Choose action:")
-b1 = st.sidebar.button("Crawl data (could be very slow)"):
-b2 = st.sidebar.button("1 Day Analysis"):
-b3 = st.sidebar.button("5 Day Analysis"):
+b1 = st.sidebar.button("Crawl data (could be very slow)")
+b2 = st.sidebar.button("1 Day Analysis")
+b3 = st.sidebar.button("5 Day Analysis")
 
 try:
     st.sidebar.title("Choose action:")
