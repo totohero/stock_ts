0c0f55ed141c18730fb280c4fa22eb9eee26a7b4
totohero <totohero@gmail.com>
2023-08-14 20:38:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import crawl_sqlite3
 import analysis_1day
 import analysis_5day
 
-# 사이드바에 메뉴 버튼을 만듭니다.
-st.sidebar.title("Choose action:")
-if st.sidebar.button("Crawl data (could be very slow)"):
-    crawl_sqlite3.crawl()
-if st.sidebar.button("1 Day Analysis"):
-    analysis_1day.doit()
-if st.sidebar.button("5 Day Analysis"):
-    analysis_5day.doit()
+try:
+    st.sidebar.title("Choose action:")
+    if st.sidebar.button("Crawl data (could be very slow)"):
+        crawl_sqlite3.crawl()
+    if st.sidebar.button("1 Day Analysis"):
+        analysis_1day.doit()
+    if st.sidebar.button("5 Day Analysis"):
+        analysis_5day.doit()
+except Exception as e:
+    st.error(traceback.format_exc())
